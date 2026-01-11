1e59a195d51218141c52f48518b12cbbfdf99084
Saxon Crawford <saxon.crawford@icloud.com>
2026-01-11 18:27:49 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Tech Compass</h1>
+            <p>React version coming soon...</p>
+        </div>
+    )
+}
+
+export default App
