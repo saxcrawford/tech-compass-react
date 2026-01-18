f1a467511906f1fd83141bb1013d96aa53ab2634
Saxon Crawford <saxon.crawford@icloud.com>
2026-01-17 20:09:42 -0500

    Completed functionality for contact form and styling when no input is in form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,28 +1,77 @@
 import {Send} from "lucide-react";
+import {useState} from "react";
 
-function About() {
+function Contact() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+    const [isVisible, setIsVisible] = useState(false);
+    const [error, setErrors] = useState({});
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsVisible(false);
+        const validationErrors = {}
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (formData.name.trim() === '') {
+            validationErrors.name = 'Name is required';
+        }
+        if (formData.email === '') {
+            validationErrors.email = 'Email is required';
+        } else if (!emailRegex.test(formData.email)) {
+            validationErrors.emailFormat = 'Email is in invalid format';
+        }
+        if (formData.message.trim() === '') {
+            validationErrors.message = 'Message is required';
+        }
+
+        if (Object.keys(validationErrors).length === 0) {
+            setIsVisible(true);
+            setFormData({
+                name: '',
+                email: '',
+                message: '',
+            })
+            setErrors({})
+        } else {
+            setErrors(validationErrors);
+        }
+    }
+
     return (
         <div className="contact-content">
             <h2>Get In Touch</h2>
             <p>Have a question or suggestion? We'd love to hear from you.</p>
 
-            <form className="contact-form" noValidate>
-                <div className="form-group">
+            <form className="contact-form" noValidate onSubmit={handleSubmit}>
+                <div className={`form-group ${error.name ? 'error' : ''}`}>
                     <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" placeholder="Your name"/>
-                    <span className="error-message"></span>
+                    <input type="text" id="name" name="name" placeholder="Your name" value={formData.name} onChange={handleChange}/>
+                    <span className={`error-message ${error.name && 'visible'}`}>{error.name}</span>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${error.email || error.emailFormat ? 'error' : ''}`}>
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="you@example.com"/>
-                    <span className="error-message"></span>
+                    <input type="email" id="email" name="email" placeholder="you@example.com" value={formData.email} onChange={handleChange}/>
+                    <span className={`error-message ${error.email && 'visible'}`}>{error.email}</span>
+                    <span className={`error-message ${error.emailFormat && 'visible'}`}>{error.emailFormat}</span>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${error.message ? 'error' : ''}`}>
                     <label htmlFor="message">Message</label>
-                    <textarea id="message" name="message" rows="4" placeholder="How can we help?"></textarea>
-                    <span className="error-message"></span>
+                    <textarea id="message" name="message" rows="4" placeholder="How can we help?" value={formData.message} onChange={handleChange}></textarea>
+                    <span className={`error-message ${error.message && 'visible'}`}>{error.message}</span>
                 </div>
 
                 <button type="submit" className="submit-btn">
@@ -30,10 +79,10 @@ function About() {
                     <Send className="button-icon" />
                 </button>
 
-                <p className="form-success">Thanks for reaching out! We'll get back to you soon.</p>
+                <p className={`form-success ${isVisible ? 'visible' : ''}`}>Thanks for reaching out! We'll get back to you soon.</p>
             </form>
         </div>
     )
 }
 
-export default About;
+export default Contact;
