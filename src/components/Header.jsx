e77bf3e9e7cd8c1696d861ed0e836d5bfeb5c31e
Saxon Crawford <saxon.crawford@icloud.com>
2026-01-21 23:25:02 -0500

    started process of adding mobile menu in navbar still work in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,43 @@
-import {CompassIcon, SunIcon, MoonIcon} from "lucide-react";
+import {CompassIcon, SunIcon, MoonIcon, Menu} from "lucide-react";
 import {Link} from "react-router-dom";
 import {useTheme} from "./ThemeContext.jsx";
-import { HashLink } from 'react-router-hash-link';
+import {HashLink} from "react-router-hash-link";
+import {useState} from "react";
 
 function Header() {
-    const {darkMode, setDarkMode} = useTheme()
-    return (
-        <header className="header">
-            <nav className="nav">
-                <Link to={'/'} className="logo">
-                    <CompassIcon className="logo-icon" size={32}/>
-                    <span>TechCompass</span>
-                </Link>
-                <ul className="nav-links">
-                    <li><HashLink smooth to='/#categories'>Chart Your Course</HashLink></li>
-                    <li><HashLink smooth to='/#about'>About</HashLink></li>
-                    <li>
-                        <button className="theme-toggle" onClick={() => {setDarkMode(!darkMode)}} aria-label="Toggle dark mode">
-                            {darkMode ? <SunIcon /> : <MoonIcon />}
-                        </button>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+	const {darkMode, setDarkMode} = useTheme();
+	const [menuVisible, setMenuVisible] = useState(false);
+
+	const menuButtonClick = () => {
+		if (window.innerWidth < 780) {
+			setMenuVisible(true);
+		} else {
+			setMenuVisible(false);
+		}
+	};
+
+	return (
+		<header className="header">
+			<nav className="nav">
+				<Link to={"/"} className="logo">
+					<CompassIcon className="logo-icon" size={32}/>
+					<span>TechCompass</span>
+				</Link>
+				<ul className={`nav-links ${menuVisible ? "visible" : ""}`}>
+					<li><HashLink smooth to="/#categories">Chart Your Course</HashLink></li>
+					<li><HashLink smooth to="/#about">About</HashLink></li>
+					<li>
+						<button className="theme-toggle" onClick={() => {
+							setDarkMode(!darkMode);
+						}} aria-label="Toggle dark mode">
+							{darkMode ? <SunIcon/> : <MoonIcon/>}
+						</button>
+					</li>
+				</ul>
+				<Menu className="menu-logo" onClick={menuButtonClick}/>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
